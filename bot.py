27dbc9960296b2ce8b88ad7948a7410be0f88028
franmj94 <franmoreno194@gmail.com>
2026-03-08 04:40:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,4 +99,20 @@ def run_bot():
 
         time.sleep(60)
 
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Bot running')
+
+def run_server():
+    port = 10000
+    server = HTTPServer(('0.0.0.0', port), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server).start()
+
 run_bot()
